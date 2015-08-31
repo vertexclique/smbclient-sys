82d0cf2eab1de607387953735f697e178ba1f9a4
Mahmut Bulut <vertexclique@gmail.com>
2015-08-31 23:40:42 +0300

    make it implicit in crate

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,8 @@ use std::{option, mem, clone, default};
 use libc::{c_int, c_char, c_double, c_void, c_uchar, c_uint, c_ulong, c_ushort, size_t, time_t,
 	mode_t, ssize_t, off_t, stat, timeval};
 
+extern crate posix;
+
 static SMBC_BASE_FD        : i32 = 10000; /* smallest file descriptor returned */
 static SMBC_WORKGROUP      : i32 = 1;
 static SMBC_SERVER         : i32 = 2;
